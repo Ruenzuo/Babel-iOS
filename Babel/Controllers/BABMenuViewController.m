bf0c42eedb81bd3dc3aa6ae71e2e5897fe97757e
Renzo Crisostomo <ruenzuo@gmail.com>
2014-07-20 21:20:00 +0200

    Initial commit

---
+++ Babel/Controllers/BABMenuViewController.m
@@ -0,0 +1,40 @@
+//
+//  BABMenuViewController.m
+//  Babel
+//
+//  Created by Renzo Crisostomo on 20/07/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+#import "BABMenuViewController.h"
+
+@interface BABMenuViewController ()
+
+@end
+
+@implementation BABMenuViewController
+
+#pragma mark - View controller life cycle
+
+- (id)initWithNibName:(NSString *)nibNameOrNil
+               bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil
+                           bundle:nibBundleOrNil];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
---
+++ Babel/Supporting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Babel
+//
+//  Created by Renzo Crisostomo on 20/07/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BABAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BABAppDelegate class]));
+    }
+}
