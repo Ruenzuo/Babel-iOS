988f1c1e10dccbb2f85cc1c25663b1adbf7bab20
Renzo Crisostomo <ruenzuo@gmail.com>
2014-07-30 21:38:22 +0200

    HockeyApp configured

--- Babel/Application/BABAppDelegate.m
+++ Babel/Application/BABAppDelegate.m
@@ -8,13 +8,45 @@
 
 #import "BABAppDelegate.h"
 
+@interface BABAppDelegate ()
+
+- (void)configureHockeyApp;
+
+@end
+
 @implementation BABAppDelegate
+
+NSString * const BABHockeyAppIdentifier = @"6fd9e830e7744e983eb60925d91d3d93";
 
 #pragma mark - Application Life cycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [self configureHockeyApp];
     return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    if( [[BITHockeyManager sharedHockeyManager].authenticator handleOpenURL:url
+                                                          sourceApplication:sourceApplication
+                                                                 annotation:annotation]) {
+        return YES;
+    }
+    return NO;
+}
+
+#pragma mark - Private Methods
+
+- (void)configureHockeyApp
+{
+    [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:BABHockeyAppIdentifier];
+    [[[BITHockeyManager sharedHockeyManager] authenticator] setIdentificationType:BITAuthenticatorIdentificationTypeDevice];
+    [[BITHockeyManager sharedHockeyManager] startManager];
+    [[[BITHockeyManager sharedHockeyManager] authenticator] authenticateInstallation];
 }
 
 @end
