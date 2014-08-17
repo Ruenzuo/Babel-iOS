416fd2f7d742776b33805e0bb5693c82f9ed8ea3
Renzo Crisostomo <ruenzuo@gmail.com>
2014-08-17 12:37:18 +0200

    BABDeveloperTableViewController implemented

---
+++ Babel/Controllers/BABDeveloperTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BABDeveloperTableViewController.h
+//  Babel
+//
+//  Created by Renzo Crisostomo on 17/08/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BABDeveloperTableViewController : UITableViewController
+
+@end
